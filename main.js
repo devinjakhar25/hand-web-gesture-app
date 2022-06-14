0472b8eb7c10258fc20fb7be0942186dc2b41eef
devinjakhar25 <86140179+devinjakhar25@users.noreply.github.com>
2022-06-14 04:16:29 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ Webcam.set({
     width: 350,
     height: 300,
     image_format: 'png',
-    png_quality: 90
+    png_quality: 100
 });
 
 camera = document.getElementById("camera");
@@ -30,4 +30,33 @@ function speak() {
     var speak_data = "The Prediction Is "+prediction;
     var utterThis = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
+}
+function check() {
+    img = document.getElementById("image_captured");
+    classifier.classify(img, gotResults);
+}
+
+function gotResults(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_gesture_name").innerHTML = results[0].label;
+        prediction = results[0].label;
+        speak();
+        if(results[0].label == "nice"){
+            document.getElementById("result_emoji").innerHTML = "&#128076;";
+            document.getElementById("quote").innerHTML = "Oh! Thank You For Saying The Food Is nice";
+        }
+        if(results[0].label == "thumbs up"){
+            document.getElementById("result_emoji").innerHTML = "&#128077";
+            document.getElementById("quote").innerHTML = "Thank You! for helping me";
+        }
+        }
+        if(results[0].label == "swag"){
+            document.getElementById("result_emoji").innerHTML = "&#129304;";
+            document.getElementById("quote").innerHTML = "Oh! You Are On A Party Enjoy It";
+        }
+        
 }
